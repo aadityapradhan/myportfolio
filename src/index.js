0d874abb238f678446f92824ba9c790054b5b152
Nabin-me <67369378+Nabin-me@users.noreply.github.com>
2022-03-11 23:03:08 +0545

    improved cursor, added user photo, and some subtle animations

--- src/index.js
+++ src/index.js
@@ -1,39 +1,154 @@
-import Scrollbar  from "smooth-scrollbar";
+import Scrollbar from "smooth-scrollbar";
+import { gsap } from "gsap";
+import '../src/styles/main.scss';
 
-var options = {
-    'damping': 0.3,
+
+
+
+
+//initialize scrollbar package
+const verticalScrollbar = Scrollbar.init(document.querySelector('.scrollbar'), {
+  dumping: 0.1,
+  delegateTo: document,
+});
+verticalScrollbar.setPosition(0, 0);
+verticalScrollbar.track.yAxis.element.remove();
+verticalScrollbar.track.xAxis.element.remove();
+//scrollevent
+verticalScrollbar.addListener((s) => {
+  var yScrollOffset = s.offset.y;
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", yScrollOffset > 0);
+});
+
+
+//menubtn script
+
+
+
+
+  
+
+
+
+
+
+
+
+//CURSOR ANIMATION
+const cursor = document.createElement('div');
+cursor.className = 'cursor';
+
+const cursorF = document.createElement('div');
+cursorF.className = 'cursor-f';
+
+let cursorX = 0;
+let cursorY = 0;
+let pageX = 0;
+let pageY = 0;
+let size = 8;
+let sizeF = 36;
+let followSpeed = .16;
+
+document.body.appendChild(cursor);
+document.body.appendChild(cursorF);
+
+if ('ontouchstart' in window) {
+  cursor.style.display = 'none';
+  cursorF.style.display = 'none';
 }
-Scrollbar.init(document.querySelector('#scrollbar'), options);
 
-let innerCursor = document.querySelector('.inner-cursor');
-let outerCursor = document.querySelector('.outer-cursor');
+let highlightZone = document.querySelector('.text-area');
+highlightZone.addEventListener('mouseenter', (e)=>{
+// console.log(e.clientX); 
+   
+  // console.log(e.clientX);
+     gsap.to(cursor, { scale: 10.5,});
+     gsap.to(cursorF, { scale: .2 }); 
+ 
+});
+highlightZone.addEventListener('mouseleave', (e)=>{
+  gsap.to(cursor, { scale: 1 });
+  gsap.to(cursorF, { scale: 1 });
+});
 
-document.addEventListener('mousemove', moveCursor);
+cursor.style.setProperty('--size', size + 'px');
+cursorF.style.setProperty('--size', sizeF + 'px');
 
-function moveCursor(e) {
-    let x = e.clientX;
-    let y = e.clientY;
 
-    // console.log(x, y);
-    innerCursor.style.left = `${x}px`;
-    innerCursor.style.top =  `${y}px`;
-    outerCursor.style.left = `${x}px`;
-    outerCursor.style.top =  `${y}px`;
+document.addEventListener('mouseleave', (e)=>{
+  gsap.fromTo(cursorF, {scale: 1}, {scale: 0, duration: 1});
+  gsap.fromTo(cursor, {scale: 1}, {scale: 0, duration: 1});
+  
+});
+document.addEventListener('mouseenter', (e)=>{
+  gsap.fromTo(cursorF, {scale: 0}, {scale: 1, duration: 1});
+  gsap.fromTo(cursor, {scale: 0}, {scale: 1, duration: 1});
+});
+
+
+window.addEventListener('mousemove', function (e) {
+  pageX = e.clientX;
+  pageY = e.clientY;
+  cursor.style.left = e.clientX - size / 2 + 'px';
+  cursor.style.top = e.clientY - size / 2 + 'px';
+  cursorF.style.opacity = .5;
+  
+  
+  
+}, );
+
+
+function loop() {
+  cursorX = lerp(cursorX, pageX, followSpeed);
+  cursorY = lerp(cursorY, pageY, followSpeed);
+  
+  
+
+  cursorF.style.top = cursorY - sizeF / 2 + 'px';
+  cursorF.style.left = cursorX - sizeF / 2 + 'px';
+ 
+  requestAnimationFrame(loop);
 }
 
-let highlightTexts = Array.from(document.querySelectorAll('.special-highlight'));
+loop();
+function lerp(start, end, amt) {
+  return (1 - amt) * start + amt * end
+};
+//custom code here
+
+let clicked = false;
+if ('ontouchstart' in window) {
+
+}
+else {
+  function mousedown(e) {
+    gsap.to(cursor, { scale: 4.5 });
+    gsap.to(cursorF, { scale: .2 });
+
+    clicked = true;
+  }
+
+  function mouseup(e) {
+    gsap.to(cursor, { scale: 1 });
+    gsap.to(cursorF, { scale: 1 });
+
+   clicked = false;
+  }
+  window.addEventListener('mousedown', mousedown, false);
+  window.addEventListener('touchstart', mousedown, false);
+  window.addEventListener('touchend', mouseup, false);
+  window.addEventListener('mouseup', mouseup, false);
 
 
+};
 
-
-let specialTxt = Array.from(document.querySelectorAll('.special-highlight'));
-
-specialTxt.forEach((txt)=> {
-    txt.addEventListener('mouseover', ()=>{
-    innerCursor.classList.add("grow");
-    });
-
-    txt.addEventListener('mouseleave', ()=>{
-        innerCursor.classList.remove("grow");
-    })
+let bodycontent = document.querySelector(".bodyview");
+var loader = document.querySelector('.loader_container');
+window.addEventListener('load', function () {
+  this.setTimeout(load_preloader, 3000);
+  function load_preloader() {
+    loader.style.display = 'none';
+    gsap.fromTo(bodycontent, {opacity: 0}, {opacity: 1, duration: 1});
+  }
 });
