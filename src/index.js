75b397f8f13db610f970298689187059fccf59de
Aaditya Pradhan <69385486+aadityapradhan@users.noreply.github.com>
2022-09-21 15:15:51 -0700

    commit for update
    impro in header, social icons, client works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Scrollbar from "smooth-scrollbar";
-import { gsap } from "gsap";
 import '../src/styles/main.scss';
-import easing from 'easing-js';
+
 
 
 
@@ -19,16 +18,6 @@ verticalScrollbar.track.xAxis.element.remove();
 
 
 
-function scrollAchievement() {
-  verticalScrollbar.scrollTo(0, 300, 700);
-  // verticalScrollbar.scrollTo(0, 100, 600, {
-  //   callback: () => {},
-  //   easing: easing.easeOutBack,
-  // });
-}
-let scrollbtn = document.querySelector('.look-btn');
-scrollbtn.addEventListener('click', scrollAchievement);
-//menubtn script
 
 
 
@@ -36,126 +25,13 @@ scrollbtn.addEventListener('click', scrollAchievement);
 
 
 
-
-
-
-
-
-// //CURSOR ANIMATION
-// const cursor = document.createElement('div');
-// cursor.className = 'cursor';
-
-// const cursorF = document.createElement('div');
-// cursorF.className = 'cursor-f';
-
-// let cursorX = 0;
-// let cursorY = 0;
-// let pageX = 0;
-// let pageY = 0;
-// let size = 8;
-// let sizeF = 36;
-// let followSpeed = .16;
-
-// document.body.appendChild(cursor);
-// document.body.appendChild(cursorF);
-
-// if ('ontouchstart' in window) {
-//   cursor.style.display = 'none';
-//   cursorF.style.display = 'none';
-// }
-
-// let highlightZone = document.querySelector('.text-area');
-// highlightZone.addEventListener('mouseenter', (e) => {
-// console.log(e.clientX); 
-
-//   // console.log(e.clientX);
-//   gsap.to(cursor, { scale: 10.5, });
-//   gsap.to(cursorF, { scale: .2 });
-
-// });
-// highlightZone.addEventListener('mouseleave', (e) => {
-//   gsap.to(cursor, { scale: 1 });
-//   gsap.to(cursorF, { scale: 1 });
-// });
-
-// cursor.style.setProperty('--size', size + 'px');
-// cursorF.style.setProperty('--size', sizeF + 'px');
-
-
-// document.addEventListener('mouseleave', (e) => {
-//   gsap.fromTo(cursorF, { scale: 1 }, { scale: 0, duration: 1 });
-//   gsap.fromTo(cursor, { scale: 1 }, { scale: 0, duration: 1 });
-
-// });
-// document.addEventListener('mouseenter', (e) => {
-//   gsap.fromTo(cursorF, { scale: 0 }, { scale: 1, duration: 1 });
-//   gsap.fromTo(cursor, { scale: 0 }, { scale: 1, duration: 1 });
-// });
-
-
-// window.addEventListener('mousemove', function (e) {
-//   pageX = e.clientX;
-//   pageY = e.clientY;
-//   cursor.style.left = e.clientX - size / 2 + 'px';
-//   cursor.style.top = e.clientY - size / 2 + 'px';
-//   cursorF.style.opacity = .5;
-
-
-
-// },);
-
-
-// function loop() {
-//   cursorX = lerp(cursorX, pageX, followSpeed);
-//   cursorY = lerp(cursorY, pageY, followSpeed);
-
-
-
-//   cursorF.style.top = cursorY - sizeF / 2 + 'px';
-//   cursorF.style.left = cursorX - sizeF / 2 + 'px';
-
-//   requestAnimationFrame(loop);
-// }
-
-// loop();
-// function lerp(start, end, amt) {
-//   return (1 - amt) * start + amt * end
-// };
-// //custom code here
-
-// let clicked = false;
-// if ('ontouchstart' in window) {
-
-// }
-// else {
-//   function mousedown(e) {
-//     gsap.to(cursor, { scale: 4.5 });
-//     gsap.to(cursorF, { scale: .2 });
-
-//     clicked = true;
+// let bodycontent = document.querySelector(".bodyview");
+// var loader = document.querySelector('.loader_container');
+// window.addEventListener('load', function () {
+//   // this.setTimeout(load_preloader, 3000);
+//   load_preloader();
+//   function load_preloader() {
+//     loader.style.display = 'none';
+//     gsap.fromTo(bodycontent, { opacity: 0 }, { opacity: 1, duration: 1 });
 //   }
-
-//   function mouseup(e) {
-//     gsap.to(cursor, { scale: 1 });
-//     gsap.to(cursorF, { scale: 1 });
-
-//     clicked = false;
-//   }
-//   window.addEventListener('mousedown', mousedown, false);
-//   window.addEventListener('touchstart', mousedown, false);
-//   window.addEventListener('touchend', mouseup, false);
-//   window.addEventListener('mouseup', mouseup, false);
-
-
-// };
-
-let bodycontent = document.querySelector(".bodyview");
-var loader = document.querySelector('.loader_container');
-window.addEventListener('load', function () {
-  // this.setTimeout(load_preloader, 3000);
-  load_preloader();
-  function load_preloader() {
-    loader.style.display = 'none';
-    gsap.fromTo(bodycontent, { opacity: 0 }, { opacity: 1, duration: 1 });
-  }
-});
+// });
